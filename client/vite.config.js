34d04cbc5802f48ccd6f8a590fea5a5bf50c8e0e
Samyak <samyak22442@iiitd.ac.in>
2025-05-04 03:09:37 +0530

    update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/', // 👈 Add this line
   plugins: [react()],
-})
+});
